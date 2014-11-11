7a60fd945cae8af4abf1b0c39898bababbe73b7b
Raymond <raymond.lcz@gmail.com>
2014-11-11 22:53:53 +0800

    Add Category UnitTest

---
+++ src/UnitTest/Test_Category.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Zee.Sample.CaveatEmptor.Model;
+using NHibernate;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class Test_Category
+    {
+        private ISessionFactory _sessionFactory;
+        private NHibernate.Cfg.Configuration _configuration;
+
+        public Test_Category()
+        {
+            _configuration = new NHibernate.Cfg.Configuration();
+            _configuration.Configure();
+            _sessionFactory = _configuration.BuildSessionFactory();
+        }
+
+        [TestCategory("Category")]
+        [TestMethod]
+        public void Can_Add_Category()
+        {
+            using (ISession session = _sessionFactory.OpenSession())
+            {
+                using (session.BeginTransaction())
+                {
+                    Category computer = new Category("Computer");
+                    session.Save(computer);
+                    session.Transaction.Commit();
+                }
+            }
+        }
+
+        [TestCategory("Category")]
+        [TestMethod]
+        public void Can_Add_ChildCategory()
+        {
+            using (ISession session = _sessionFactory.OpenSession())
+            {
+                using (session.BeginTransaction())
+                {
+                    Category computer = session.Get<Category>(1L);
+
+                    Category laptops = new Category("Laptops");
+                    laptops.ParentCategory = computer;
+                    computer.ChildCategories.Add(laptops);
+
+                    
+                    session.Transaction.Commit();
+                }
+            }
+        }
+
+        [TestCategory("Category")]
+        [TestMethod]
+        public void Can_Add_ChildCategory_OutSession()
+        {
+            Category electronics;
+            using (ISession session = _sessionFactory.OpenSession())
+            {
+                using (session.BeginTransaction())
+                {
+                    electronics = new Category("Electronics");
+                    session.Save(electronics);
+                    session.Transaction.Commit();
+                }
+            }
+
+            Category cellPhone = new Category("Cell Phones");
+            electronics.ChildCategories.Add(cellPhone);
+            cellPhone.ParentCategory = electronics;
+
+            using (ISession session = _sessionFactory.OpenSession())
+            {
+                using (session.BeginTransaction())
+                {
+                    session.Save(cellPhone);
+                    session.Transaction.Commit();
+                }
+            }
+        }
+
+        [TestCategory("Category")]
+        [TestMethod]
+        public void Can_Delete_Category()
+        {
+            using (ISession session = _sessionFactory.OpenSession())
+            {
+                using (session.BeginTransaction())
+                {
+                    Category computer = session.Get<Category>(1L);
+                    session.Delete(computer);
+                    session.Transaction.Commit();
+                }
+            }
+        }
+
+    }
+}
